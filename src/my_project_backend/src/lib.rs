bc2dd23920718e0d02694e5ae23b2b9eb43581ba
Laura Strycharczuk <u21_laustr_lbn@technischools.com>
2024-12-04 19:50:44 +0000

    Added config

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,19 +1,45 @@
 use std::cell::RefCell;
+
 use crate::blog::Blog;
+use crate::config::Config;
+
 mod blog;
+mod config;
 
 thread_local! {
+    static CONFIG: RefCell<Config> = RefCell::new(Config::new());
     static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<String, String> {
-    if title.len() > 250 {
-        return Err("Title is too long".to_string());
+fn add_config(new_config: Config) {
+    CONFIG.with(|conf| *conf.borrow_mut() = new_config);
+}
+
+#[ic_cdk::update]
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
+    let config = CONFIG.with(|config| config.borrow().clone());
+    if title.len() > config.max_title_len as usize {
+        return Err("Title is too long!".to_string())
+    }
+    if content.len() > config.max_content_len as usize {
+        return Err("Content is too long!".to_string())
+    }
+    if tags.len() > config.max_tags_count as usize {
+        return Err("Too many tags!".to_string())
+    }
+    let are_tags_in_config_tags = tags.iter().any(|tag| config.tags.contains(tag));
+    if !are_tags_in_config_tags {
+        return Err("Some tags are not allowed!".to_string())
     }
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
-    Ok("Added new blog".to_string())
+    let last_blog = BLOGS.with(|blogs| 
+        blogs
+        .borrow()
+        .last()
+        .expect("Vec should not be empty").clone());
+    Ok(last_blog)
 }
 
 #[ic_cdk::query]
